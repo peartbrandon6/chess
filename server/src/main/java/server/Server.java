63a146df38cf403a4e5305cd8af34d6444a34ae5
Bando <peartbrandon6@gmail.com>
2025-10-08 15:03:58 -0600

    Got static files test working so I can test my other stuff now

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,24 +1,32 @@
 package server;
 
 import io.javalin.*;
+import io.javalin.http.Context;
 
 public class Server {
 
-    private final Javalin javalin;
+    private final Javalin server;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        server = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
-
+        server.delete("db", ctx -> ctx.result("{}"));
+        server.post("user", this::register);
     }
 
+    private void register(Context ctx){
+        ctx.result("{\"username\":\"joe\", \"authToken\":\"xyz\"}");
+    }
+
+
+
     public int run(int desiredPort) {
-        javalin.start(desiredPort);
-        return javalin.port();
+        server.start(desiredPort);
+        return server.port();
     }
 
     public void stop() {
-        javalin.stop();
+        server.stop();
     }
 }

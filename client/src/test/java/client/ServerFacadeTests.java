a7214776ddb21f7febb96e4c9d6c4a8fddb1b579
Brandon Peart <peartbrandon6@gmail.com>
2025-11-16 01:12:11 -0700

    changed port number and removed another import

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -17,7 +17,7 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init() {
         server = new Server();
-        facade = new ServerFacade("http://localhost:8080");
+        facade = new ServerFacade("http://localhost:0");
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
     }
